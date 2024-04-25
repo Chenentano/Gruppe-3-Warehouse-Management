3a8892343b222eae5e991ef43d40410da4bbc9e0
SilviaGuillen <silviaguillens@gmail.com>
2024-04-25 17:16:14 +0200

    frontend 1.draft

---
+++ frontend/src/components/ProductCard.tsx
@@ -0,0 +1,22 @@
+import {Product} from "../App.tsx";
+import {Link} from "react-router-dom";
+import '../assets/styles.css';
+
+
+
+
+
+type ProductProps = {
+    product: Product
+    showBackToMainPage: boolean
+}
+export default function ProductCard({product, showBackToMainPage}:ProductProps) {
+    return <li key={product.id}>
+        <h2>{product.name}</h2>
+        <h3>Category: {product.category}</h3>
+        <h4>Quantity: {product.quantity}</h4>
+        {showBackToMainPage ? <Link to={"/"}>Got back to main page </Link> :
+            <Link to={"/product/" + product.id}>Show product detail page</Link>}
+    </li>
+}
+
---
+++ frontend/src/pages/productsPage.tsx
@@ -0,0 +1,22 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {Product} from "../App.tsx";
+import ProductCard from "../components/ProductCard.tsx";
+import axios from "axios";
+
+
+
+export default function ProductsPage() {
+
+    const [product, setProduct] = useState<Product>({})
+
+    const params = useParams()
+
+    useEffect(() => {
+        axios.get("https://localhost:8080/api/product/" + params.id).then(response => {
+            setProduct(response.data)
+        })
+    }, [params.id])
+
+    return <><ProductCard product={product} showBackToMainPage={true} /></>
+}
