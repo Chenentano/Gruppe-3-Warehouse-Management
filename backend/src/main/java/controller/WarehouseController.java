8d70482bd3f13ae03301964bff19fea353f2722c
SilviaGuillen <silviaguillens@gmail.com>
2024-04-25 12:08:13 +0200

    update product

--- backend/src/main/java/controller/WarehouseController.java
+++ backend/src/main/java/controller/WarehouseController.java
@@ -1,4 +1,38 @@
 package controller;
 
+import lombok.RequiredArgsConstructor;
+import model.Product;
+import org.springframework.web.bind.annotation.*;
+import service.WarehouseService;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+@RequestMapping("Api/product")
+@RestController
+@RequiredArgsConstructor
 public class WarehouseController {
+
+    private final WarehouseService service;
+
+    @GetMapping
+    public List<Product> getAllProducts() {return service.getAllProducts();}
+
+    @PostMapping
+    public Product saveNewProduct(@RequestBody Product newProduct) {return service.saveNewProduct(newProduct);}
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@RequestBody Product requiredProduct) {return service.updateProduct(requiredProduct);}
+
+    @DeleteMapping
+    public String deleteProduct(@PathVariable String id) {
+        try {
+            service.deleteProduct(id).orElseThrow();
+            return "Product not deleted";
+        }catch (NoSuchElementException e) {
+            return "Product deleted ok";
+        }
+    }
+
+
 }
--- backend/src/main/java/service/WarehouseService.java
+++ backend/src/main/java/service/WarehouseService.java
@@ -1,4 +1,37 @@
 package service;
 
+import model.Product;
+import repository.WarehouseRepository;
+
+import java.util.List;
+import java.util.Optional;
+
 public class WarehouseService {
+    private final WarehouseRepository repo;
+
+    public List<Product> getAllProducts(){return repo.findAll();}
+
+    public Product saveNewProduct(Product newProduct) {
+        Product product = new Product(
+                newProduct.id(),
+                newProduct.productId(),
+                newProduct.productName(),
+                newProduct.productQuantity()
+                newProduct.category(),
+        ); repo.save(product);
+        return product;
+    }
+
+    public Product updateProduct(Product requiredProduct) {
+        Product toUpdate = repo.findById(requiredProduct.getId()).orElseThrow();
+        repo.save(toUpdate.withStatus(requiredProduct.getStatus()).withDescription(requiredProduct.getDescrition()));
+        return repo.findById(toUpdate.getId()).orElseThrow();
+    }
+
+    public Optional<Product> deleteProduct(String id) {
+        repo.delete(repo.findById(id).orElseThrow());
+        return repo.findById(id);
+    }
+
+
 }
