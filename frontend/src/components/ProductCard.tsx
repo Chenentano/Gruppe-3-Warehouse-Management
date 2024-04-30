de5d9a9cdf2d401fdd26fa1570bff59cd1127930
basti <bastian_behrendt@yahoo.de>
2024-04-30 14:16:33 +0100

    add productID to ProductCard

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -11,6 +11,7 @@ export default function ProductCard({ product }: ProductProps) {
         <li className="product-card">
             <div className="product-content">
                 <h2 className="product-name">{product.productName}</h2>
+                <p className="product-category">Produkt ID: {product.productId}</p>
                 <p className="product-category">Category: {product.category}</p>
                 <p className="product-quantity">Quantity: {product.productQuantity}</p>
             </div>
