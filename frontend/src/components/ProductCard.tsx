cc3fabdda13812ecb710275e7ed95c95f91a68de
basti <bastian_behrendt@yahoo.de>
2024-04-30 15:53:09 +0100

    delete function + button hinzugefügt

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,19 +1,26 @@
-import { Product } from "../types/Product.tsx";
+import { Product } from "../types/Product";
 import "../ProductCard.css";
+import axios from "axios";
 
-type ProductProps = {
+export type ProductProps = {
     product: Product;
-    showBackToMainPage: boolean;
 };
 
 export default function ProductCard({ product }: ProductProps) {
+
+    const handleDelete = () => {
+        axios.delete("api/product/delete/" + product.id)
+            .then(() => {console.log("Klappt!")});
+    };
+
     return (
         <li className="product-card">
             <div className="product-content">
-                <h2 className="product-name">{product.productName}</h2>
-                <p className="product-category">Produkt ID: {product.productId}</p>
+                <h2 className="product-name">Product-Name{product.productName}</h2>
+                <p className="product-category">Product ID: {product.productId}</p>
                 <p className="product-category">Category: {product.category}</p>
                 <p className="product-quantity">Quantity: {product.productQuantity}</p>
+                <button onClick={handleDelete}>Delete</button>
             </div>
         </li>
     );
