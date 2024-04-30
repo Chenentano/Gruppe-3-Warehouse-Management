f6b5be0f53279cd1e995677e2119d988539da1e6
basti <bastian_behrendt@yahoo.de>
2024-04-30 12:28:12 +0100

    new styling for QuantityStatus.tsx

--- frontend/src/components/QuantityStatus.tsx
+++ frontend/src/components/QuantityStatus.tsx
@@ -5,11 +5,16 @@ type QuantityStatusProps = {
     onChange: (event:ChangeEvent<HTMLSelectElement>) => void
 }
 
-export default function QuantityStatus({onChange, status}: QuantityStatusProps) {
-    return   <select onChange={onChange} value={status}>
-        <option value={"All"} >All</option>
-        <option value={"minimum"}>over50Units</option>
-        <option value={"Null"}>NullUnits</option>
-        <option value={"unknown"}>Unknown</option>
-    </select>
+export default function QuantityStatus({ onChange, status }: QuantityStatusProps) {
+    return (
+        <div className="select-container">
+            <select className="select-status" onChange={onChange} value={status}>
+                <option value={"All"}>All</option>
+                <option value={"minimum"}>over50Units</option>
+                <option value={"Null"}>NullUnits</option>
+                <option value={"unknown"}>Unknown</option>
+            </select>
+            <div className="select-arrow"></div>
+        </div>
+    );
 }
