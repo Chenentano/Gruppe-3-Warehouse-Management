830410fd264ae49c32f02723bcc665bcbdab2b6f
Carsten Kremser <github@carstenkremser.de>
2024-05-02 16:06:39 +0200

    remove filters on startPage

--- frontend/src/pages/startPage.tsx
+++ frontend/src/pages/startPage.tsx
@@ -1,7 +1,6 @@
-import QuantityStatus from "../components/QuantityStatus.tsx";
 import ProductCard from "../components/ProductCard.tsx";
 import {Product} from "../types/Product.tsx";
-import {ChangeEvent, useEffect, useState} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
 import {FilterValues} from "../components/Filter.tsx";
 
@@ -13,8 +12,6 @@ type StartPageProps = {
 export default function StartPage(props: StartPageProps) {
 
     const [products, setProducts] = useState<Product[]>([]);
-    const [input, setInput] = useState<string>("");
-    const [status, setStatus] = useState<string>("All");
 
     useEffect(() => {
         // Fetch all products when the component mounts
@@ -27,14 +24,6 @@ export default function StartPage(props: StartPageProps) {
         }).catch(error => {
             console.error("Error fetching products:", error);
         });
-    }
-
-    function handleOnChangeInput(event: ChangeEvent<HTMLInputElement>) {
-        setInput(event.target.value);
-    }
-
-    function handleOnSelectChange(event: ChangeEvent<HTMLSelectElement>) {
-        setStatus(event.target.value);
     }
 
     function doFilter(product: Product): boolean {
@@ -58,23 +47,14 @@ export default function StartPage(props: StartPageProps) {
 
     return (
         <>
-            <header>
-                <div>
-                    <input type="text" onChange={handleOnChangeInput} value={input} />
-                    <QuantityStatus status={status} onChange={handleOnSelectChange} />
-                </div>
-            </header>
-
-        <ul>
-            {products
-                //.filter(product => status === "All" || product.category === ProductCategory.NONE && ProductCategory.GENERAL)
-                //.filter(product => product.productName.toLowerCase().includes(input.toLowerCase()))
-                .filter(product => doFilter(product))
-                .map((product: Product) => (
-                <ProductCard key={product.productId} product={product} showBackToMainPage={false}/>
-        ))}
-        </ul>
-</>
-)
-    ;
+            <ul>
+                {products
+                    .filter(product => doFilter(product))
+                    .map((product: Product) => (
+                        <ProductCard key={product.productId} product={product} showBackToMainPage={false}/>
+                    ))}
+            </ul>
+        </>
+    )
+        ;
 }
