32b0e65d1bcac9b26b2fde2e7fd76d5f1822eccf
Carsten Kremser <github@carstenkremser.de>
2024-05-03 15:26:17 +0200

    Frontend: add user info to UserBar

--- frontend/src/components/UserBar.tsx
+++ frontend/src/components/UserBar.tsx
@@ -1,6 +1,7 @@
 import './UserBar.css';
 
 type UserBarProps = {
+    user: string | undefined,
     loginFunction: () => void,
     logoutFunction: () => void
 }
@@ -21,9 +22,18 @@ export function UserBar(props: UserBarProps): JSX.Element {
         }
     }
 
+    function userInfo(): string {
+        if (props.user === undefined || props.user === null || props.user === "" || props.user === "anonymousUser") {
+            return "not logged in";
+        }
+        return "Hello " + props.user;
+    }
+
     return (<>
     <div className="user-bar">
-        <div></div>
+        <div>
+            {userInfo()}
+        </div>
         <div className="login-logout-button-container" >
             <button className="button-login-logout button-login" onClick={onLogin}>login</button>
             <button className="button-login-logout button-logout" onClick={onLogout}>logout</button>
