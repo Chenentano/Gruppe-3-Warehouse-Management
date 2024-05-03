ee8cd2886e3b4d5d1b9e15718d17b7adc3def81c
Carsten Kremser <github@carstenkremser.de>
2024-05-03 12:06:27 +0200

    add logout button and restyle UserBar

--- frontend/src/components/UserBar.tsx
+++ frontend/src/components/UserBar.tsx
@@ -1,13 +1,22 @@
 import './UserBar.css';
 
 export function UserBar() {
+
     function onLogin() {
         console.log('onLogin: Login Button pressed');
     }
+
+    function onLogout() {
+        console.log('onLogout: Logout Button pressed');
+    }
+
     return (<>
-        <div className="user-bar">
-            <h2>USER</h2>
-            <button className="button-login" onClick={() => {onLogin}}>login</button>
+    <div className="user-bar">
+        <div></div>
+        <div className="login-logout-button-container" >
+            <button className="button-login-logout button-login" onClick={() => {onLogin}}>login</button>
+            <button className="button-login-logout button-logout" onClick={() => {onLogout}}>logout</button>
         </div>
-    </>)
+    </div>
+</>)
 }
