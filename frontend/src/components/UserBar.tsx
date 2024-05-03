8a9bd4e5706e0b2d3e00115721ae5cd9b27155d0
Carsten Kremser <github@carstenkremser.de>
2024-05-03 13:09:05 +0200

    Frontend: add code for login and logout buttons

--- frontend/src/components/UserBar.tsx
+++ frontend/src/components/UserBar.tsx
@@ -1,21 +1,32 @@
 import './UserBar.css';
 
-export function UserBar() {
+type UserBarProps = {
+    loginFunction: () => void,
+    logoutFunction: () => void
+}
+
+export function UserBar(props: UserBarProps): JSX.Element {
 
     function onLogin() {
         console.log('onLogin: Login Button pressed');
+        if (props.loginFunction) {
+            props.loginFunction();
+        }
     }
 
     function onLogout() {
         console.log('onLogout: Logout Button pressed');
+        if (props.logoutFunction) {
+            props.logoutFunction();
+        }
     }
 
     return (<>
     <div className="user-bar">
         <div></div>
         <div className="login-logout-button-container" >
-            <button className="button-login-logout button-login" onClick={() => {onLogin}}>login</button>
-            <button className="button-login-logout button-logout" onClick={() => {onLogout}}>logout</button>
+            <button className="button-login-logout button-login" onClick={onLogin}>login</button>
+            <button className="button-login-logout button-logout" onClick={onLogout}>logout</button>
         </div>
     </div>
 </>)
